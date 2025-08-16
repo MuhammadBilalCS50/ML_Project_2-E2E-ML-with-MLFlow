f16cad0683c5024dbad0f1d50043dcadfb24cb20
MuhammadBilalCS50 <mbilalshahid777@gmail.com>
2025-08-17 02:32:35 +0500

    model evaluation

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 from ml_project_2 import logger
 from ml_project_2.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ml_project_2.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from ml_project_2.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from ml_project_2.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from ml_project_2.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/ml_project_2/config/configuration.py
+++ src/ml_project_2/config/configuration.py
@@ -1,6 +1,6 @@
 from ml_project_2.constants import *
 from ml_project_2.utils.common import read_yaml, create_directories
-from ml_project_2.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from ml_project_2.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -69,3 +69,22 @@ class ConfigurationManager:
 
         return model_trainer_config
     
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/MuhammadBilalCS50/ML_Project_2-E2E-ML-with-MLFlow.mlflow",
+           
+        )
+
+        return model_evaluation_config
